b3695f492e6683dbaee39437a92b434690e57a2d
AlexanderGonchar <goncharas24@gmail.com>
2022-09-04 09:43:52 +0300

    add z2

---
+++ z2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine();
+int[] array = new int[10];
+Random rand = new Random();
+for (int i = 0; i < 10; i++)
+{
+    array[i] = rand.Next(1, 100);
+    Console.Write($"{i}: {array[i]};   ");
+}
+int NumberMin = 0;
+int NumberMax = 9;
+int productOfNumbers = 0;
+Console.WriteLine();
+for (int j = 1; NumberMin < NumberMax; j++)
+{
+    productOfNumbers = array[NumberMax] * array[NumberMin];
+    Console.Write($"{j} пара чисел = {productOfNumbers};  ");
+    NumberMax = NumberMax - 1;
+    NumberMin++;
+}
